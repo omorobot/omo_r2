b9dec6b4fd416aad6f759ccf903755a78802837b
t-shaped-person <t.shaped.person@gmail.com>
2024-10-03 09:55:46 +0900

    modify omo_r2_teleop

--- omo_r2_teleop/setup.py
+++ omo_r2_teleop/setup.py
@@ -1,13 +1,13 @@
 import os # add
 from glob import glob # add
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'omo_r2_teleop'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
