2d5490f74f3c8e7ff0cbea4d2b461eb1dc5986fd
t-shaped-person <t.shaped.person@gmail.com>
2024-10-06 21:55:11 +0900

    create omo_r2_description

---
+++ omo_r2_description/launch/description_rviz.launch.py
@@ -0,0 +1,43 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    description_dir = get_package_share_directory('omo_r2_description')
+
+    rviz_file = LaunchConfiguration('rviz_file', default=os.path.join(description_dir, 'rviz', 'description.rviz'))
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    rviz_file_arg = DeclareLaunchArgument('rviz_file', default_value=rviz_file)
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=use_sim_time)
+
+    with open(os.path.join(description_dir, 'urdf', os.environ['ROBOT_MODEL'] + '.urdf'), 'r') as infp:
+        robot_description = infp.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}, {'use_sim_time': use_sim_time}]
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_file],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen',
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(rviz_file_arg)
+    ld.add_action(use_sim_time_arg)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(rviz2_node)
+
+    return ld
